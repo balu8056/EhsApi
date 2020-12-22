c19f4ff3c14ae36ad583b695f1398cf462a0bd35
Balasubramani R <46841670+balu8056@users.noreply.github.com>
2020-12-22 14:40:08 +0530

    ...

--- api.js
+++ api.js
@@ -19,13 +19,17 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 
+app.use("/assets/uploads", express.static('uploads'));
+
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
   res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
+  if (req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    return res.status(200).json({});
+  }
   next();
 });
 
